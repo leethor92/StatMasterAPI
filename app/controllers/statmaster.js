92956f95b5ebaf0d1715eb12f875e2c23368bd96
DESKTOP-TMDJO97\Lee <leethornton1992@hotmail.com>
2021-01-24 22:03:35 +0000

    basic structure & some images implemented

---
+++ app/controllers/statmaster.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const Statmaster = {
+  index: {
+    handler: function(request, h) {
+      return h.file('./app/views/main.html');
+    }
+  },
+  signup: {
+    handler: function(request, h) {
+      return h.file('./app/views/signup.html')
+    }
+  },
+  login: {
+    handler: function(request, h) {
+      return h.file('./app/views/login.html')
+    }
+  }
+}
+
+module.exports = Statmaster;
--- routes.js
+++ routes.js
@@ -1,3 +1,8 @@
-const Controller = require('./controller.js');
+const Statmaster = require('./app/controllers/statmaster.js');
 
-module.exports = [{ method: 'GET', path: '/', config: Controller.index }];
+module.exports = [
+  { method: 'GET', path: '/', config: Statmaster.index },
+  { method: 'GET', path: '/signup', config: Statmaster.signup },
+  { method: 'GET', path: '/login', config: Statmaster.login },
+  {method: "GET", path: '/{param*}', handler: {directory: {path:'./public'}}}
+  ];
