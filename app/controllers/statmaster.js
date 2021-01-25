f03c0f2d81ea2b326f88312466bb0bdbcbcafd8b
DESKTOP-TMDJO97\Lee <leethornton1992@hotmail.com>
2021-01-25 21:53:35 +0000

    implemented handlebars

--- app/controllers/statmaster.js
+++ app/controllers/statmaster.js
@@ -3,17 +3,17 @@
 const Statmaster = {
   index: {
     handler: function(request, h) {
-      return h.file('./app/views/main.html');
+      return h.view('main', {title: 'Welcome to Statmaster'});
     }
   },
   signup: {
     handler: function(request, h) {
-      return h.file('./app/views/signup.html')
+      return h.view('signup', {title: 'Sign up for Statmaster'});
     }
   },
   login: {
     handler: function(request, h) {
-      return h.file('./app/views/login.html')
+      return h.view('login', {title: 'Login to Statmaster'});
     }
   }
 }
